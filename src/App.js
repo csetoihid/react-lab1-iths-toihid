b85574cca12646b9e455426365cd96818fc6a92c
csetoihid <cse.toihid@gmail.com>
2020-10-22 06:11:40 +0200

    update events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 import Header from './components/Header';
@@ -9,26 +8,8 @@ function App() {
   return (
     <div className="App" >
       <header className="App-header">
-
-      
-
-
       <Header></Header>
       <QuizContent length='3'></QuizContent>
-
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/components/QuizContent.js
+++ src/components/QuizContent.js
@@ -1,59 +1,56 @@
-import React from 'react';
-import logo from '../logo.svg';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+//import PropTypes from 'prop-types';
 import signInstruction from '../images/road-signs-instruction.png';
 import signDirection from '../images/signDirection.svg';
 import signWarning from '../images/signWarning.svg';
 
 const QuizContent = (props) => {
     const ran = Math.floor(Math.random() * props.length);
+    const [check, setCheck] = useState('row');
+    const quizes = [
+    {
+        "name": "Road sign",
+        "id": 1,
+        "question": signInstruction,
+        "answers": ['Mandatory Sign', 'Direction Sign', 'Instruction Sign', 'Prohibitory Sign'],
+        "correct": 2,
+    },
+    {
+        "name": 'Road sign',
+        "id": 2,
+        "question": signDirection,
+        "answers": [ 'Direction Sign', 'Instruction Sign', 'Mandatory Sign', 'Prohibitory Sign'],
+        "correct": 0
+    },
+    {
+        "name": 'Road sign',
+        "id": 3,
+        "question": signWarning,
+        "answers": [ 'Warning sign', 'Instruction Sign', 'Mandatory Sign', 'Prohibitory Sign'],
+        "correct": 0,
+    }
 
+    ];
 
-const quizes = [
- {
-    "name": "Road sign",
-    "id": 1,
-    "question": signInstruction,
-    "answers": ['Mandatory Sign', 'Direction Sign', 'Instruction Sign', 'Prohibitory Sign'],
-    "correct": 2,
-  },
-  {
-    "name": 'Road sign',
-    "id": 2,
-    "question": signDirection,
-    "answers": [ 'Direction Sign', 'Instruction Sign', 'Mandatory Sign', 'Prohibitory Sign'],
-    "correct": 0
-  },
-  {
-    "name": 'Road sign',
-    "id": 3,
-    "question": signWarning,
-    "answers": [ 'Warning sign', 'Instruction Sign', 'Mandatory Sign', 'Prohibitory Sign'],
-    "correct": 0,
-  }
-
-];
-
-const givenAnswer = (event) => {
-    var clickedAnswer = event.target.getAttribute('index');
-    if(clickedAnswer == quizes[ran].correct){
-        alert('correct');
-    } else {
-        alert('wrong');
-    } 
-
-}
+    const givenAnswer = (event) => {
+        var clickedAnswer = event.target.getAttribute('index');
+        if(Number(clickedAnswer) === quizes[ran].correct){
+            setCheck('row true');
+        }else{
+            setCheck('row false');
+        }
+    }
 
     return (
-        <div className="quiz-content container">
-            <div className= 'row'>
-                <div className='col-4'>
+        <div className= "quiz-content container">
+            <div className= {check}>
+                <div className='col-4 pt-4'>
                     <img classname='img-responsive' src={quizes[ran].question} className="App-logo" alt="logo" />
                 </div>
                 <div className= "col-8">
                 <ul>
                     {quizes[ran].answers.map((value, index) => {
-                        return <li onClick={givenAnswer} index={index}>  {value}  {index} </li>
+                        return <li onClick={givenAnswer} index={index}>  {value}</li>
                     })}
                 </ul>
 
